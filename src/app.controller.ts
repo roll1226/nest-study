379e5a1e490c65f1764477ee39d1d0c82a038e38
roll1226 <tomo_yasu_4724@icloud.com>
2021-04-08 00:28:26 +0900

    feat: query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('query')
+  queries(@Query('id') id: string) {
+    return { id };
   }
 }
